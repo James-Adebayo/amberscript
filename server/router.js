d8b75fe71dd46004d90c63371f87a556476b42bc
James <redbarcompany+render@gmail.com>
2026-07-12 11:14:55 -0700

    updated wallet and investments

---
+++ server/router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require("path");
+// require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
+const verified = false;
+
+const routes = {
+    "/": "index.html",
+    "/auth" : "auth/index.html",
+    "/tasks": "tasks/",
+    "/wallet" : "wallet/index.html"
+}
+
+app.use((req, res, next) => {
+    if (req.path.length > 1 && req.path.endsWith("/")){
+        return res.redirect(301, req.path.slice(0, -1));
+    }
+    next();
+});
+
+app.get("/", protectedRoute, (req, res) => {
+    const file = routes[req.path];
+
+    if (file) {
+        res.sendFile(path.join(__dirname, "frontend", file));
+    }else{
+        res.status(400).sendFile(__dirname, "frontend", "404.html");
+    }
+});
+
+function protectedRoute(req, res, next){
+    if (!verified){
+        return res.redirect('/auth');
+    }
+    next();
+}
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
