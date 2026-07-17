fe10f5ce9c4c504558d5edbb664dd69b7012845f
James <redbarcompany+render@gmail.com>
2026-07-16 18:54:32 -0700

    im getting tired

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // app.use(removeTrailingSlash);
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(process.cwd(), "frontend")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", webRoutes);
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,22 +1,22 @@
 const path = require("path");
 class PageController {
     home(req, res) {
-        res.sendFile(path.join(__dirname, "../frontend/index.html"));
+        res.sendFile(path.join(process.cwd(), "frontend/index.html"));
     }
     tasks(req, res) {
-        res.sendFile(path.join(__dirname, "../frontend/tasks/index.html"));
+        res.sendFile(path.join(process.cwd(), "frontend/tasks/index.html"));
     }
 
     auth(req, res) {
-        res.sendFile(path.join(__dirname, "../frontend/auth/index.html"));
+        res.sendFile(path.join(process.cwd(), "frontend/auth/index.html"));
     };
 
     wallet(req, res) {
-        res.sendFile(path.join(__dirname, "../frontend/wallet/index.html"));
+        res.sendFile(path.join(process.cwd(), "frontend/wallet/index.html"));
     };
 
     confirmEmail(req, res) {
-        res.sendFile(path.join(__dirname, "../frontend/confirm-email/index.html"))
+        res.sendFile(path.join(process.cwd(), "frontend/confirm-email/index.html"))
     }
 }
 
